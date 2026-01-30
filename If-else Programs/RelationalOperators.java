ae2399d5106f6bdbc4423a165d4cd54df0943d5f
Krishnasingh9999 <krishna1863singh@gmail.com>
2026-01-30 15:04:11 +0530

    If Else programs and logics

---
+++ If-else Programs/RelationalOperators.java
@@ -0,0 +1,12 @@
+public class RelationalOperators {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 2;
+    System.out.println(x > y); //true (Boolean data type)
+    System.out.println(5 == 5); //true
+    System.out.println(11 < 10); //false
+    System.out.println(11 != 10); //true
+    System.out.println(99 != 99); //false
+    System.out.println( 8 <= 8); //true
+  }
+}
